f3a44948a44c4e73b1205e3229d294f2ccccb6be
rpungello <3287663+rpungello@users.noreply.github.com>
2025-09-22 18:39:30 +0000

    Fix styling

--- src/ChangelogParser.php
+++ src/ChangelogParser.php
@@ -10,8 +10,8 @@ class ChangelogParser
     /**
      * Parses the changelog file and returns a Changelog object.
      *
-     * @param string $path The path to the changelog file.
-     * @param Driver|null $driver Optional driver instance for parsing. Defaults to StandardDriver if not provided.
+     * @param  string  $path  The path to the changelog file.
+     * @param  Driver|null  $driver  Optional driver instance for parsing. Defaults to StandardDriver if not provided.
      * @return Changelog A Changelog object containing parsed data.
      */
     public static function parseChangelog(string $path, ?Driver $driver = null): Changelog
@@ -24,9 +24,9 @@ class ChangelogParser
     /**
      * Parses the changelog file and returns a Changelog object filtering changes after a specific version.
      *
-     * @param string $path The path to the changelog file.
-     * @param string|null $afterVersion The version after which changes should be included.
-     * @param Driver|null $driver Optional driver instance for parsing. Defaults to StandardDriver if not provided.
+     * @param  string  $path  The path to the changelog file.
+     * @param  string|null  $afterVersion  The version after which changes should be included.
+     * @param  Driver|null  $driver  Optional driver instance for parsing. Defaults to StandardDriver if not provided.
      * @return Changelog A Changelog object containing filtered parsed data.
      */
     public static function parseChangelogAfter(string $path, ?string $afterVersion = null, ?Driver $driver = null): Changelog
